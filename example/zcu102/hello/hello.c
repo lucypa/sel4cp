535a17d9374ea8daf1c5228d4d072b8855272fc3
Lucy <lucyparker544@gmail.com>
2022-08-10 11:41:58 +1000

    Trivial: Clean up
    Signed-off-by: Lucy <lucyparker544@gmail.com>

--- example/zcu102/hello/hello.c
+++ example/zcu102/hello/hello.c
@@ -6,34 +6,13 @@
 #include <stdint.h>
 #include <sel4cp.h>
 
-uintptr_t shmem_vaddr;
-
-static void
-mycpy(char *dst, char *src, unsigned int length)
-{   
-
-    int i = 0;
-    while (i < length) {
-        dst[i] = src[i];
-        i++;
-    }
-}
-
 void
 init(void)
 {
-    char *shello = "hello";
-    sel4cp_dbg_puts("starting...\n");
-
-    mycpy((char*)shmem_vaddr, shello, 5);
-
-    sel4cp_notify(0);
+    sel4cp_dbg_puts("hello, world\n");
 }
 
 void
 notified(sel4cp_channel ch)
 {
-    sel4cp_dbg_puts("notified\n");
-    sel4cp_dbg_puts((char *)shmem_vaddr);
-    sel4cp_dbg_puts("\n");
 }
